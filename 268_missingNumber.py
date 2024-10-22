08921d34228315ba367aca9468c22462c71fd992
likeai22 <mvrad79@yahoo.com>
2024-10-23 04:29:02 +0500

    :recycle: Complete missingNumber solution

--- 268_missingNumber.py
+++ 268_missingNumber.py
@@ -2,6 +2,26 @@ from typing import List
 
 
 class Solution:
+    def radix_sort(self, arr: List[int]) -> List[int]:  # https://thecode.media/radix/
+        max_digits = max([len(str(x)) for x in arr])
+
+        base = 10
+        bins = [[] for _ in range(base)]
+        for i in range(0, max_digits):
+            for x in arr:
+                digit = (x // base**i) % base
+                bins[digit].append(x)
+            arr = [x for queue in bins for x in queue]
+            bins = [[] for _ in range(base)]
+        return arr
+
+    def missingNumber_radix(self, nums: List[int]) -> int:  # beats 95.4
+        nums[:] = self.radix_sort(nums)
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+        return len(nums)
+
     @staticmethod
     def missingNumber(nums: List[int]) -> int:  # beats 97.07
         nums = sorted(nums)
