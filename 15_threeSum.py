c18c26c48b63967b12d7654710e621797e56cc3c
likeai22 <mvrad79@yahoo.com>
2024-10-24 03:29:29 +0500

    :hammer: Solving ThreeSum with the wrong answer

--- 15_threeSum.py
+++ 15_threeSum.py
@@ -20,6 +20,44 @@ class Solution:
                     right -= 1
                 summ = nums[left] + nums[right]
                 if summ + nums[i] == 0:
+                    key = tuple([nums[i], nums[left], nums[right]])
+                    if key not in unique_triplets:
+                        unique_triplets.add(key)
+                        result.append(list(key))
+                if summ + nums[i] > 0:
+                    right -= 1
+                else:
+                    left += 1
+            left += 1
+            right -= 1
+        return result
+
+
+    @staticmethod
+    def threeSum(nums: List[int]) -> List[List[int]]:
+        result = []
+        unique_triplets = set()
+        nums.sort()
+
+        for i in range(len(nums)):
+            left = i + 1
+            right = len(nums) - 1
+            if len(set(nums)) > 1 and nums[i] == nums[i - 1]:
+                continue
+            while left < right:
+                summ = nums[i] + nums[left] + nums[right]
+                # if left == i:
+                #     left += 1
+                # elif right == i:
+                #     right -= 1
+                # summ = nums[left] + nums[right]
+                if summ == 0:
+                    key = ([nums[i], nums[left], nums[right]])
+                    if key not in unique_triplets:
+                        unique_triplets.add(key)
+                        result.append(list(key))
+
+                if summ + nums[i] == 0:
                     key = tuple(sorted([nums[i], nums[left], nums[right]]))
                     if key not in unique_triplets:
                         unique_triplets.add(key)
