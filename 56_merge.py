00a78560526a2a6f154b47ed4fdcf4aeabb63ada
likeai22 <mvrad79@yahoo.com>
2024-10-24 20:26:30 +0500

    :recycle: Solving Merge

--- 56_merge.py
+++ 56_merge.py
@@ -4,12 +4,24 @@ from typing import List
 class Solution:
     @staticmethod
     def merge(intervals: List[List[int]]) -> List[List[int]]:
-        result = []
-        pass
-        return result
+        intervals.sort()
+        i = 0
+        while i < len(intervals) - 1:
+            if (
+                intervals[i][0] <= intervals[i + 1][0]
+                and intervals[i][1] >= intervals[i + 1][1]
+            ):
+                del intervals[i + 1]
+                continue
+            if intervals[i][1] >= intervals[i + 1][0]:
+                intervals[i][1] = intervals[i + 1][1]
+                del intervals[i + 1]
+                continue
+            i += 1
+        return intervals
 
 
 if __name__ == "__main__":
-    intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+    intervals = [[1, 4], [0, 2], [3, 5]]
     Solution.merge(intervals)
     print(intervals)
