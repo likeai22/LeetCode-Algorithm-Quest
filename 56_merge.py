b8ef3d2e16902ac92f6cfe1d4274e703bbc38c94
likeai22 <mvrad79@yahoo.com>
2024-10-24 06:13:40 +0500

    :tada: Merge Intervals problem beginning

---
+++ 56_merge.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def merge(intervals: List[List[int]]) -> List[List[int]]:
+        result = []
+        pass
+        return result
+
+
+if __name__ == "__main__":
+    intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+    Solution.merge(intervals)
+    print(intervals)
